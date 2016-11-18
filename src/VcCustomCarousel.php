943e40f3eb3e6cf57ec1dab3c9ddc6a48bb90a9f
Leandro Gonçalves <contato.leandrogoncalves@gmail.com>
2016-11-18 18:55:39 -0200

    fix features

--- src/VcCustomCarousel.php
+++ src/VcCustomCarousel.php
@@ -187,8 +187,8 @@ class VcCustomCarousel {
 					$(".vc_cc").slick({
 						dots: true,
 						infinite: true,
-						slidesToShow: 3,
-						slidesToScroll: 3
+						slidesToShow: 2,
+						slidesToScroll: 2
 					});
 				});
 			</script>
